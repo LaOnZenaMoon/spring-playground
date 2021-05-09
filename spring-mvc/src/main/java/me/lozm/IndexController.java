7eebdb7274575e1d92b5c87bad26d89b08a6074a
junlee <laonzenamoon@gmail.com>
2021-05-09 14:13:44 +0900

    feat & refactor: spring-mvc 모듈 추가

--- spring-mvc/src/main/java/me/lozm/IndexController.java
+++ spring-mvc/src/main/java/me/lozm/IndexController.java
@@ -1,14 +1,14 @@
-package me.lozm;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class IndexController {
-
-    @GetMapping
-    public String index() {
-        return "index";
-    }
-
-}
+//package me.lozm;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class IndexController {
+//
+//    @GetMapping
+//    public String index() {
+//        return "index";
+//    }
+//
+//}
